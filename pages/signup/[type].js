250289ea7799b51e18ea0e187a96509019e42a0f
Churchil Owino <chachowino75@gmail.com>
2023-04-04 14:57:22 +0300

    Create the sign up type route compnent

---
+++ pages/signup/[type].js
@@ -0,0 +1,11 @@
+function SignUpTypePage() {
+  return (
+    <section>
+      <div>
+        <h1>This is the Sign Up Type Page</h1>
+      </div>
+    </section>
+  );
+}
+
+export default SignUpTypePage;
