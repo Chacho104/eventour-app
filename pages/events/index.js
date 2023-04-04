30d8a72a4a34710cce25df65df68526e3384970a
Churchil Owino <chachowino75@gmail.com>
2023-04-04 14:56:51 +0300

    Create the all events page route component

---
+++ pages/events/index.js
@@ -0,0 +1,11 @@
+function AllEventsPage() {
+  return (
+    <section>
+      <div>
+        <h1>This is the All Events Page</h1>
+      </div>
+    </section>
+  );
+}
+
+export default AllEventsPage;
