72336be4efeee56d1426382c6fc4b4abde9dada6
Churchil Owino <chachowino75@gmail.com>
2023-04-04 14:56:34 +0300

    Create the event detail page route component

---
+++ pages/events/[eventId].js
@@ -0,0 +1,11 @@
+function EventDetailPage() {
+  return (
+    <section>
+      <div>
+        <h1>This is the Event Detail Page</h1>
+      </div>
+    </section>
+  );
+}
+
+export default EventDetailPage;
