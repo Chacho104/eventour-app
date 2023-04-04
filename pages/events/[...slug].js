0b9bfef3d33aee98b9c88680886d929d403be144
Churchil Owino <chachowino75@gmail.com>
2023-04-04 14:56:17 +0300

    Create the filter route for events

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+function FilteredEventsPage() {
+  return (
+    <section>
+      <div>
+        <h1>This is the Filtered Events Page</h1>
+      </div>
+    </section>
+  );
+}
+
+export default FilteredEventsPage;
