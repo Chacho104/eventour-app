2b35a57a4778cb32fe51abbfa98ff43c9370ed3f
Churchil Owino <chachowino75@gmail.com>
2023-04-05 20:10:15 +0300

    Replace initial boilerplate with created components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
+import FeaturedEvents from "@/components/events/featured-events";
+import Hero from "@/components/layout/hero";
+import MainNav from "@/components/layout/main-nav";
+import { Fragment } from "react";
+
 function HomePage() {
   return (
-    <section>
-      <div>
-        <h1>This is the Home Page</h1>
-      </div>
-    </section>
+    <Fragment>
+      <MainNav />
+      <main>
+        <Hero />
+        <FeaturedEvents />
+      </main>
+    </Fragment>
   );
 }
 
