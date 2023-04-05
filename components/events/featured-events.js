d40e8ea4962d49a24ab9b9bcb8394440bc133fa4
Churchil Owino <chachowino75@gmail.com>
2023-04-05 20:12:04 +0300

    Create the featured events component boilerplate

---
+++ components/events/featured-events.js
@@ -0,0 +1,10 @@
+import classes from "./featured-events.module.css";
+function FeaturedEvents() {
+  return (
+    <section className={classes["featured-events"]}>
+      <h1>Upcoming Events</h1>
+    </section>
+  );
+}
+
+export default FeaturedEvents;
