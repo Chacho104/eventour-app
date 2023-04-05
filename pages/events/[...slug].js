35971b39899d669090acd7e04ab091b535292d38
Churchil Owino <chachowino75@gmail.com>
2023-04-05 20:10:05 +0300

    Replace initial boilerplate with created components

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,10 +1,16 @@
+import MainNav from "@/components/layout/main-nav";
+import { Fragment } from "react";
+
 function FilteredEventsPage() {
   return (
-    <section>
-      <div>
-        <h1>This is the Filtered Events Page</h1>
-      </div>
-    </section>
+    <Fragment>
+      <MainNav />
+      <main>
+        <div>
+          <h1>This is the Filtered Events Page</h1>
+        </div>
+      </main>
+    </Fragment>
   );
 }
 
