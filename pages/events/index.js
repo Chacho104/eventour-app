12518db634cbd7710571eca5dc7f8f94bcb8939d
Churchil Owino <chachowino75@gmail.com>
2023-04-05 20:09:29 +0300

    Use created components in page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,10 +1,16 @@
+import MainNav from "@/components/layout/main-nav";
+import { Fragment } from "react";
+
 function AllEventsPage() {
   return (
-    <section>
-      <div>
-        <h1>This is the All Events Page</h1>
-      </div>
-    </section>
+    <Fragment>
+      <MainNav />
+      <main>
+        <div>
+          <h1>This is the all events page...</h1>
+        </div>
+      </main>
+    </Fragment>
   );
 }
 
