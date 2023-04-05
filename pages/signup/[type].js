06327368ce10189b61a3223740e3b71c7666cb5f
Churchil Owino <chachowino75@gmail.com>
2023-04-05 20:08:59 +0300

    Use components in page

--- pages/signup/[type].js
+++ pages/signup/[type].js
@@ -1,10 +1,16 @@
+import MainNav from "@/components/layout/main-nav";
+import { Fragment } from "react";
+
 function SignUpTypePage() {
   return (
-    <section>
-      <div>
-        <h1>This is the Sign Up Type Page</h1>
-      </div>
-    </section>
+    <Fragment>
+      <MainNav />
+      <main>
+        <div>
+          <h1>This is the Sign Up Type Page</h1>
+        </div>
+      </main>
+    </Fragment>
   );
 }
 
