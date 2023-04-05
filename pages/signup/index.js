dfdc7b20723ff0e1298fd3bc6c75c05ef5441d23
Churchil Owino <chachowino75@gmail.com>
2023-04-05 20:08:32 +0300

    Use components in sign up page index

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,10 +1,16 @@
+import MainNav from "@/components/layout/main-nav";
+import { Fragment } from "react";
+
 function SignUpPage() {
   return (
-    <section>
-      <div>
-        <h1>This is the Sign Up Page</h1>
-      </div>
-    </section>
+    <Fragment>
+      <MainNav />
+      <main>
+        <div>
+          <h1>This is the Sign Up Page</h1>
+        </div>
+      </main>
+    </Fragment>
   );
 }
 
