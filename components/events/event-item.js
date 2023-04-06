57dc366bf77a82b66f3ba8f0e4b2320423a82144
Churchil Owino <chachowino75@gmail.com>
2023-04-06 15:14:25 +0300

    Create the event item component

---
+++ components/events/event-item.js
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import classes from "./event-item.module.css";
+
+function EventItem(props) {
+  const { image, title, date, category, id } = props;
+  const formattedDate = new Date(date);
+  const eventDay = formattedDate.getDate();
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const eventMonth = month[formattedDate.getMonth()];
+
+  const exploreLink = `/events/${id}`;
+  return (
+    <li className={classes.event}>
+      <Link href={exploreLink}>
+        <img src={"/" + image} alt={title} />
+        <div className={classes.date}>
+          <p>{eventDay}</p>
+          <p>{eventMonth}</p>
+        </div>
+        <div className={classes.details}>
+          <h3>{category}</h3>
+          <h2>{title}</h2>
+          <span>320 Participants</span>
+        </div>
+      </Link>
+    </li>
+  );
+}
+
+export default EventItem;
