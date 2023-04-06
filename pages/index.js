fb5b910d031f34e3445d25501c810e3a36759804
Churchil Owino <chachowino75@gmail.com>
2023-04-06 15:19:14 +0300

    Add the footer component to home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
-import FeaturedEvents from "@/components/events/featured-events";
 import Hero from "@/components/layout/hero";
 import MainNav from "@/components/layout/main-nav";
+import { getFeaturedEvents } from "@/dummy-data";
 import { Fragment } from "react";
+import EventList from "@/components/events/event-list";
+import Process from "@/components/layout/process";
+import Footer from "@/components/layout/footer/footer";
 
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <Fragment>
       <MainNav />
       <main>
         <Hero />
-        <FeaturedEvents />
+        <Process />
+        <section>
+          <h1>Upcoming Events</h1>
+          <EventList events={featuredEvents} />
+        </section>
       </main>
+      <Footer />
     </Fragment>
   );
 }
