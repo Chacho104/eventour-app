cb019fae40319f13b8c4e4ba6ee067460ce0d00b
Churchil Owino <chachowino75@gmail.com>
2023-04-06 15:17:11 +0300

    Create the copyright component for footer

---
+++ components/layout/footer/copyright.js
@@ -0,0 +1,9 @@
+import classes from "./copyright.module.css";
+function Copyright() {
+  return (
+    <section className={classes.copyright}>
+      <span>&copy; Eventour 2023 | All Rights Reserved</span>
+    </section>
+  );
+}
+export default Copyright;
