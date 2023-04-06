a2405787fc7b995f45079d64fbfd9d979fe4e298
Churchil Owino <chachowino75@gmail.com>
2023-04-06 15:17:43 +0300

    Create the footer component

---
+++ components/layout/footer/footer.js
@@ -0,0 +1,15 @@
+import Copyright from "./copyright";
+import SocialMediaLinks from "./social-media";
+import UsefulLinks from "./useful-links";
+
+function Footer() {
+  return (
+    <footer>
+      <SocialMediaLinks />
+      <UsefulLinks />
+      <Copyright />
+    </footer>
+  );
+}
+
+export default Footer;
