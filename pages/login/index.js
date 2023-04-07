ef285807d6ba88d8a1da88206fb4b214f2544bbb
Churchil Owino <chachowino75@gmail.com>
2023-04-07 11:58:32 +0300

    Refactor page layout

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,17 +1,9 @@
-import Footer from "@/components/layout/footer/footer";
-import MainNav from "@/components/layout/main-nav";
 import { Fragment } from "react";
 
 function LoginPage() {
   return (
     <Fragment>
-      <MainNav />
-      <main>
-        <div>
-          <h1>This is the Login Page</h1>
-        </div>
-      </main>
-      <Footer />
+      <h1>This is the Login Page</h1>
     </Fragment>
   );
 }
