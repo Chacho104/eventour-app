03e65206527890c1c1c062fb9c22b971875282f1
Churchil Owino <chachowino75@gmail.com>
2023-04-07 12:02:28 +0300

    Build the layout component

---
+++ components/layout/layout.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import MainNav from "./main-nav";
+import Footer from "./footer/footer";
+
+function Layout(props) {
+  return (
+    <Fragment>
+      <MainNav />
+      <main>{props.children}</main>
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default Layout;
