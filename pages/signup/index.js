4b8a182fa107ed6da6ef10b99bc5a47c034abcab
Churchil Owino <chachowino75@gmail.com>
2023-04-07 11:58:03 +0300

    Refactor page layout

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -1,17 +1,9 @@
-import Footer from "@/components/layout/footer/footer";
-import MainNav from "@/components/layout/main-nav";
 import { Fragment } from "react";
 
 function SignUpPage() {
   return (
     <Fragment>
-      <MainNav />
-      <main>
-        <div>
-          <h1>This is the Sign Up Page</h1>
-        </div>
-      </main>
-      <Footer />
+      <h1>This is the Sign Up Page</h1>
     </Fragment>
   );
 }
