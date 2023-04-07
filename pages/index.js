632f88d4d90a8410b6d54a0f44c6cfb5da9694fd
Churchil Owino <chachowino75@gmail.com>
2023-04-07 11:59:38 +0300

    Refactor page layput to remove redundancy

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,19 @@
 import Hero from "@/components/layout/hero";
-import MainNav from "@/components/layout/main-nav";
 import { getFeaturedEvents } from "@/dummy-data";
 import { Fragment } from "react";
 import EventList from "@/components/events/event-list";
 import Process from "@/components/layout/process";
-import Footer from "@/components/layout/footer/footer";
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
   return (
     <Fragment>
-      <MainNav />
-      <main>
-        <Hero />
-        <Process />
-        <section>
-          <h1>Upcoming Events</h1>
-          <EventList events={featuredEvents} />
-        </section>
-      </main>
-      <Footer />
+      <Hero />
+      <Process />
+      <section>
+        <h1>Upcoming Events</h1>
+        <EventList events={featuredEvents} />
+      </section>
     </Fragment>
   );
 }
