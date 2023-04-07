77dbd7c66f98a34c971040ff699470fb0c2f7280
Churchil Owino <chachowino75@gmail.com>
2023-04-07 11:58:54 +0300

    Refactor page layput to remove redundancy

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,17 +1,9 @@
-import Footer from "@/components/layout/footer/footer";
-import MainNav from "@/components/layout/main-nav";
 import { Fragment } from "react";
 
 function AllEventsPage() {
   return (
     <Fragment>
-      <MainNav />
-      <main>
-        <div>
-          <h1>This is the all events page...</h1>
-        </div>
-      </main>
-      <Footer />
+      <h1>This is the all events page...</h1>
     </Fragment>
   );
 }
