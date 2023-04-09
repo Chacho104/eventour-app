56b686963462f79a535a2bc65817e45335eb6ff9
Churchil Owino <chachowino75@gmail.com>
2023-04-09 17:09:08 +0300

    Use button generic component Revise h1 element

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { getFeaturedEvents } from "@/dummy-data";
 import { Fragment } from "react";
 import EventList from "@/components/events/event-list";
 import Process from "@/components/layout/process";
+import Button from "@/components/ui/button";
 
 function HomePage() {
   const featuredEvents = getFeaturedEvents();
@@ -11,8 +12,9 @@ function HomePage() {
       <Hero />
       <Process />
       <section>
-        <h1>Upcoming Events</h1>
+        <h1>Top Upcoming Events</h1>
         <EventList events={featuredEvents} />
+        <Button link="/events">Browse more events</Button>
       </section>
     </Fragment>
   );
