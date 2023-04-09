92aafdbcf36e1d9e05949d205f4634e7135fd19b
Churchil Owino <chachowino75@gmail.com>
2023-04-09 17:02:38 +0300

    Create the event logistics component

---
+++ components/event-detail/event-logistics.js
@@ -0,0 +1,61 @@
+import { TbCalendarTime } from "react-icons/tb";
+import { VscLocation } from "react-icons/vsc";
+import { MdOutlinePhone } from "react-icons/md";
+import { FaMoneyBillWave } from "react-icons/fa";
+import classes from "./event-logistics.module.css";
+
+function EventLogistics(props) {
+  const { date, location, time, contact, price } = props;
+  const readableDate = new Date(date).toLocaleString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  let cost;
+  if (+price === 0) {
+    cost = "Free";
+  } else {
+    cost = `$${price}`;
+  }
+
+  return (
+    <section className={classes.logistics}>
+      <div className={classes["logs-item"]}>
+        <div className={classes.icon}>
+          <TbCalendarTime />
+        </div>
+        <div>
+          <p>{readableDate}</p>
+          <p>{time}</p>
+        </div>
+      </div>
+      <div className={classes["logs-item"]}>
+        <div className={classes.icon}>
+          <VscLocation />
+        </div>
+        <div>
+          <p>{location}</p>
+        </div>
+      </div>
+      <div className={classes["logs-item"]}>
+        <div className={classes.icon}>
+          <MdOutlinePhone />
+        </div>
+        <div>
+          <p>{contact}</p>
+        </div>
+      </div>
+      <div className={classes["logs-item"]}>
+        <div className={classes.icon}>
+          <FaMoneyBillWave />
+        </div>
+        <div>
+          <p>{cost}</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default EventLogistics;
