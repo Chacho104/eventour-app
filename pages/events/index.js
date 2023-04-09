8e3ddcf51ec3066e2ad13d5849e3e2cea760bcdf
Churchil Owino <chachowino75@gmail.com>
2023-04-09 16:59:20 +0300

    Use events search and events list components

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,24 @@
+import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
+import { getAllEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 import { Fragment } from "react";
 
 function AllEventsPage() {
+  const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month, category, cost) {
+    const fullPath = `/events/${year}/${month}/${category}`;
+    router.push(fullPath);
+  }
   return (
     <Fragment>
-      <h1>This is the all events page...</h1>
+      <EventsSearch onSearch={findEventsHandler} />
+      <section>
+        <h1>All Upcoming Events</h1>
+        <EventList events={events} />
+      </section>
     </Fragment>
   );
 }
