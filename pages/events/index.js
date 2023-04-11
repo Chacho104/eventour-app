da68c414695ded9bcec858257f91113c70760576
Churchil Owino <chachowino75@gmail.com>
2023-04-11 11:05:45 +0300

    Add SSG to page using getStaticProps Add Head component to page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,19 +1,27 @@
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
-import { getAllEvents } from "@/dummy-data";
+import { getAllEvents } from "@/components/helpers/api-util";
 import { useRouter } from "next/router";
 import { Fragment } from "react";
+import Head from "next/head";
 
-function AllEventsPage() {
-  const events = getAllEvents();
+function AllEventsPage(props) {
+  const { events } = props;
   const router = useRouter();
 
-  function findEventsHandler(year, month, category, cost) {
+  function findEventsHandler(year, month, category) {
     const fullPath = `/events/${year}/${month}/${category}`;
     router.push(fullPath);
   }
   return (
     <Fragment>
+      <Head>
+        <title>All Events</title>
+        <meta
+          name="description"
+          content="Find a lot of events that will spice up your life!"
+        />
+      </Head>
       <EventsSearch onSearch={findEventsHandler} />
       <section>
         <h1>All Upcoming Events</h1>
@@ -21,6 +29,17 @@ function AllEventsPage() {
       </section>
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default AllEventsPage;
