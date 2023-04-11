5a5a32aa050a0ee2c69e4d56cbe3d53d9b77b58c
Churchil Owino <chachowino75@gmail.com>
2023-04-11 11:11:08 +0300

    Replace img element with Image component for optimization

--- components/event-detail/event-summary.js
+++ components/event-detail/event-summary.js
@@ -1,10 +1,18 @@
 import classes from "./event-summary.module.css";
+import Image from "next/image";
+
 function EventSummary(props) {
   const { image, category, title } = props;
   return (
     <section>
       <div className={classes.image}>
-        <img src={"/" + image} alt={title} />
+        <Image
+          src={"/" + image}
+          alt={title}
+          fill
+          sizes="(max-width: 560px) 100vw, (max-width: 304px) 50vw, 33vw"
+          priority
+        />
       </div>
       <div className="details">
         <h3>{category}</h3>
