179914eb310c360f4cfebe22d7d84c3fc4489475
Churchil Owino <chachowino75@gmail.com>
2023-04-11 11:08:26 +0300

    Add Head component to _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import Layout from "@/components/layout/layout";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>Eventour Events</title>
+        <meta name="description" content="Great events for you to enjoy!" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
