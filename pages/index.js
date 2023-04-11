c3f21d2c52033f6f2e6e9d5f8b6aa42574edfb78
Churchil Owino <chachowino75@gmail.com>
2023-04-11 11:07:39 +0300

    Add SSG to page using getStaticProps Add Head component to page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,40 @@
 import Hero from "@/components/layout/hero";
-import { getFeaturedEvents } from "@/dummy-data";
+import { getFeaturedEvents } from "@/components/helpers/api-util";
 import { Fragment } from "react";
 import EventList from "@/components/events/event-list";
 import Process from "@/components/layout/process";
 import Button from "@/components/ui/button";
+import Head from "next/head";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
   return (
     <Fragment>
+      <Head>
+        <title>Eventour</title>
+        <meta
+          name="description"
+          content="Find a lot of events that will spice up your life!"
+        />
+      </Head>
       <Hero />
       <Process />
       <section>
         <h1>Top Upcoming Events</h1>
-        <EventList events={featuredEvents} />
+        <EventList events={props.events} />
         <Button link="/events">Browse more events</Button>
       </section>
     </Fragment>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
