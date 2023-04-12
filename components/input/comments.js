0cd1ffec9abe8c50950b3cc7293aa6e3a4c12f2f
Churchil Owino <chachowino75@gmail.com>
2023-04-12 10:50:05 +0300

    Add comments component

---
+++ components/input/comments.js
@@ -0,0 +1,86 @@
+import { useContext, useEffect, useState } from "react";
+
+import CommentList from "./comment-list";
+import NewComment from "./new-comment";
+import classes from "./comments.module.css";
+import NotificationContext from "@/store/notification-context";
+
+function Comments(props) {
+  const { eventId } = props;
+
+  const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+  const [isFetchingComments, setIsFetchingComments] = useState();
+
+  const notificationCtx = useContext(NotificationContext);
+
+  useEffect(() => {
+    if (showComments) {
+      setIsFetchingComments(true);
+      fetch("/api/comments/" + eventId)
+        .then((response) => response.json())
+        .then((data) => {
+          setComments(data.comments);
+          setIsFetchingComments(false);
+        });
+    }
+  }, [showComments]);
+
+  function toggleCommentsHandler() {
+    setShowComments((prevStatus) => !prevStatus);
+  }
+
+  function addCommentHandler(commentData) {
+    // send data to API
+    notificationCtx.showNotification({
+      title: "Sending comment...",
+      message: "Adding your comment to the database.",
+      status: "pending",
+    });
+
+    fetch("/api/comments/" + eventId, {
+      method: "POST",
+      body: JSON.stringify(commentData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong!");
+        });
+      })
+      .then((data) =>
+        notificationCtx.showNotification({
+          title: "Success!",
+          message: "Your comment was added.",
+          status: "success",
+        })
+      )
+      .catch((error) =>
+        notificationCtx.showNotification({
+          title: "Error!",
+          message: error.message || "Something went wrong!",
+          status: "error",
+        })
+      );
+  }
+
+  return (
+    <section className={classes.comments}>
+      <button className={classes["toggle-btn"]} onClick={toggleCommentsHandler}>
+        {showComments ? "Hide" : "Show"} Comments
+      </button>
+      {showComments && !isFetchingComments && (
+        <CommentList commentItems={comments} />
+      )}
+      {showComments && isFetchingComments && <p>Loading comments...</p>}
+      {showComments && <NewComment onAddComment={addCommentHandler} />}
+    </section>
+  );
+}
+
+export default Comments;
