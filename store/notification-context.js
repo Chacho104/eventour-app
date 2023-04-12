eae3e23224c79238e8bc50da82bf0c4fec8e405f
Churchil Owino <chachowino75@gmail.com>
2023-04-12 10:55:53 +0300

    Create the notification-context api

---
+++ store/notification-context.js
@@ -0,0 +1,48 @@
+import { createContext, useEffect, useState } from "react";
+
+const NotificationContext = createContext({
+  notification: null, // { title, message, status }
+  showNotification: function (notificationData) {},
+  hideNotification: function () {},
+});
+
+export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  useEffect(() => {
+    if (
+      activeNotification &&
+      (activeNotification.status === "success" ||
+        activeNotification.status === "error")
+    ) {
+      const timer = setTimeout(() => {
+        setActiveNotification(null);
+      }, 3000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [activeNotification]);
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const notification = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
+  return (
+    <NotificationContext.Provider value={notification}>
+      {props.children}
+    </NotificationContext.Provider>
+  );
+}
+
+export default NotificationContext;
