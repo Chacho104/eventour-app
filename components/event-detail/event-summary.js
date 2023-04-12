de297bd19d47282a3d2211018d7761016a20010b
Churchil Owino <chachowino75@gmail.com>
2023-04-12 10:48:43 +0300

    Revise component to include title and category only

--- components/event-detail/event-summary.js
+++ components/event-detail/event-summary.js
@@ -1,23 +1,12 @@
 import classes from "./event-summary.module.css";
-import Image from "next/image";
 
 function EventSummary(props) {
-  const { image, category, title } = props;
+  const { category, title } = props;
   return (
     <section>
-      <div className={classes.image}>
-        <Image
-          src={"/" + image}
-          alt={title}
-          fill
-          sizes="(max-width: 560px) 100vw, (max-width: 304px) 50vw, 33vw"
-          priority
-        />
-      </div>
-      <div className="details">
-        <h3>{category}</h3>
-        <h2>{title}</h2>
-        <span>320 Participants</span>
+      <div className={classes.title}>
+        <h1>{title}</h1>
+        <h2>{category} event</h2>
       </div>
     </section>
   );
