c376a04fb7873bff1a546a1ae7ca92f3e1f2cfff
Churchil Owino <chachowino75@gmail.com>
2023-04-12 10:53:55 +0300

    Wrap everything with the context provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import Layout from "@/components/layout/layout";
+import { NotificationContextProvider } from "@/store/notification-context";
 import "@/styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Eventour Events</title>
-        <meta name="description" content="Great events for you to enjoy!" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Eventour Events</title>
+          <meta name="description" content="Great events for you to enjoy!" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
