8e244fe19372a66694ac6daf9a1249c79b9b6584
Churchil Owino <chachowino75@gmail.com>
2023-04-12 10:50:46 +0300

    Add new comment component

---
+++ components/input/new-comment.js
@@ -0,0 +1,64 @@
+import { useRef, useState } from "react";
+import classes from "./new-comment.module.css";
+import Button from "../ui/button";
+
+function NewComment(props) {
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  const emailInputRef = useRef();
+  const nameInputRef = useRef();
+  const commentInputRef = useRef();
+
+  function sendCommentHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredName = nameInputRef.current.value;
+    const enteredComment = commentInputRef.current.value;
+
+    if (
+      !enteredEmail ||
+      enteredEmail.trim() === "" ||
+      !enteredEmail.includes("@") ||
+      !enteredName ||
+      enteredName.trim() === "" ||
+      !enteredComment ||
+      enteredComment.trim() === ""
+    ) {
+      setIsInvalid(true);
+      return;
+    }
+
+    props.onAddComment({
+      email: enteredEmail,
+      name: enteredName,
+      text: enteredComment,
+    });
+  }
+
+  return (
+    <section className={classes["post-comment"]}>
+      <h2>Leave a comment</h2>
+      <form className={classes.form} onSubmit={sendCommentHandler}>
+        <div className={classes.row}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your email</label>
+            <input type="email" id="email" ref={emailInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your name</label>
+            <input type="text" id="name" ref={nameInputRef} />
+          </div>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="comment">Your comment</label>
+          <textarea id="comment" rows="5" ref={commentInputRef}></textarea>
+        </div>
+        {isInvalid && <p>Please enter a valid email address and comment!</p>}
+        <Button>Submit</Button>
+      </form>
+    </section>
+  );
+}
+
+export default NewComment;
