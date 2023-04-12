f1734366270ac3c454c79c7af490d77491144d7b
Churchil Owino <chachowino75@gmail.com>
2023-04-12 10:52:36 +0300

    Add notification component to layout

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,23 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import Notification from "../ui/notification";
 import MainNav from "./main-nav";
 import Footer from "./footer/footer";
+import NotificationContext from "@/store/notification-context";
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+
   return (
     <Fragment>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
       <MainNav />
       <main>{props.children}</main>
       <Footer />
