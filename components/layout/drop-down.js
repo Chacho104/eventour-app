1d888653582a1f0561edee82ee7873903de944ed
Churchil Owino <chachowino75@gmail.com>
2023-04-25 11:39:42 +0300

    Create the drop down menu cmp

---
+++ components/layout/drop-down.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import classes from "./drop-down.module.css";
+
+function DropDown(props) {
+  return (
+    <nav className={classes["drop-down"]}>
+      <ul>
+        <li>
+          <Link href="/" onClick={props.onClick}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/about" onClick={props.onClick}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link href="/events" onClick={props.onClick}>
+            All Events
+          </Link>
+        </li>
+        <li>
+          <Link href="/login" onClick={props.onClick}>
+            Log In
+          </Link>
+        </li>
+        <li>
+          <Link href="/signup" onClick={props.onClick}>
+            Sign Up
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default DropDown;
