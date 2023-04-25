a656eabb9c5eb675ca9d03a5acbcd2d8247df5fa
Churchil Owino <chachowino75@gmail.com>
2023-04-25 11:35:24 +0300

    Add dynamic route

---
+++ pages/events/[slug].js
@@ -0,0 +1,71 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import Button from "@/components/ui/button";
+import ErrorAlert from "@/components/ui/error-alert";
+import { getEventData, getFeaturedEvents } from "@/components/helpers/api-util";
+import Head from "next/head";
+import Comments from "@/components/input/comments";
+import { Fragment } from "react";
+
+export const dynamic = "force-dynamic";
+
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
+
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event found!</p>
+        <Button link="/events">Show all events</Button>
+      </ErrorAlert>
+    );
+  }
+  return (
+    <Fragment>
+      <Head>
+        <title>{event.title}</title>
+        <meta name="description" content={event.description} />
+      </Head>
+      <EventSummary category={event.category} title={event.title} />
+      <EventLogistics
+        title={event.title}
+        date={event.date}
+        location={event.location}
+        time={event.time}
+        contact={event.contact}
+        price={event.price}
+        image={event.image}
+      />
+      <EventContent description={event.description} />
+      <Comments eventId={event._id} />
+      <Button>Get your ticket</Button>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+  const event = await getEventData(slug);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths() {
+  // Instead of pre-rendering all events, we could just pre-render the featured events.
+  const events = await getFeaturedEvents();
+  const paths = events.map((event) => ({ params: { slug: event.slug } }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export default EventDetailPage;
