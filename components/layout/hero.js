752836ac478974d846a65198b9222395db363b11
Churchil Owino <chachowino75@gmail.com>
2023-04-25 11:39:12 +0300

    Revise hero h1 content

--- components/layout/hero.js
+++ components/layout/hero.js
@@ -5,9 +5,7 @@ function Hero() {
   return (
     <section>
       <div className={classes.hero}>
-        <h1>
-          Where event goers and event creators meet faster, for greater fun
-        </h1>
+        <h1>Where event goers and event creators meet, faster</h1>
         <NewsletterRegistration />
       </div>
     </section>
