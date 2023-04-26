dfa6bbd4c091c522e8b4e259956d723ed129d850
Churchil Owino <chachowino75@gmail.com>
2023-04-26 13:19:44 +0300

    Remove connection to local host api routes

--- components/helpers/api-util.js
+++ components/helpers/api-util.js
@@ -1,21 +1,3 @@
-export async function getAllEvents() {
-  const response = await fetch("http://localhost:3000/api/events");
-  const data = await response.json();
-
-  const events = data.events;
-  return events;
-}
-
-export async function getFeaturedEvents() {
-  const allEvents = await getAllEvents();
-  return allEvents.filter((event) => event.isFeatured);
-}
-
-export async function getEventData(slug) {
-  const allEvents = await getAllEvents();
-  return allEvents.find((event) => event.slug === slug);
-}
-
 export async function getFilteredEvents(eventFilter) {
   const allEvents = await getAllEvents();
   const { year, month, category } = eventFilter;
