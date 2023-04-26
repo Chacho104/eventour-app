e6b2946d96ef8e0ec49e0eb10d04287a75cdfbb0
Churchil Owino <chachowino75@gmail.com>
2023-04-26 15:29:53 +0300

    Add another year to select year options

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -34,6 +34,7 @@ function EventsSearch(props) {
               <option value="">Select year</option>
               <option value="2023">2023</option>
               <option value="2024">2024</option>
+              <option value="2025">2025</option>
             </select>
           </div>
           <div className={classes.control}>
