01b6a5a2c09ba5a30f305daad552d5b8f196c074
Churchil Owino <chachowino75@gmail.com>
2023-04-26 22:07:52 +0300

    Define hero image

--- components/layout/hero.js
+++ components/layout/hero.js
@@ -1,12 +1,20 @@
-import NewsletterRegistration from "../input/newsletter-registration";
+import Image from "next/image";
 import classes from "./hero.module.css";
 
 function Hero() {
   return (
     <section>
       <div className={classes.hero}>
-        <h1>Where event goers and event creators meet, faster</h1>
-        <NewsletterRegistration />
+        <div className={classes.image}>
+          <Image
+            src="/images/event-banner2.jpeg"
+            fill
+            alt="First event banner"
+          />
+        </div>
+        <div className={classes.content}>
+          <h1>Where event goers and event creators meet, faster</h1>
+        </div>
       </div>
     </section>
   );
