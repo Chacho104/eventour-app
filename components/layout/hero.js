8ec59037db1d07ee58277e78d8bdc2a53daff820
Churchil Owino <chachowino75@gmail.com>
2023-04-27 07:52:42 +0300

    Remove next image Use an overlay

--- components/layout/hero.js
+++ components/layout/hero.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import classes from "./hero.module.css";
 import Button from "../ui/button";
 
@@ -6,13 +5,7 @@ function Hero() {
   return (
     <section>
       <div className={classes.hero}>
-        <div className={classes.image}>
-          <Image
-            src="/images/event-banner2.jpeg"
-            fill
-            alt="First event banner"
-          />
-        </div>
+        <div className={classes.overlay}></div>
         <div className={classes.content}>
           <h1>Where event goers and event creators meet, faster</h1>
           <Button link="/events">Browse all events</Button>
